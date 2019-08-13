f06bffc99bbec38a29c39e44d50ca37672d059df
3927o <53431636+3927o@users.noreply.github.com>
2019-08-13 22:16:26 +0800

    增加箱子物件
    算法应该没问题，能不能算出来就是另一回事了

--- 崩崩崩夏活.py
+++ 崩崩崩夏活.py
@@ -13,51 +13,10 @@ Map = [[0, 2, 2, 2, 2, 2, 2, 2],
        [2, 2, 2, 2, 2, 2, 2, 2],
        [2, 2, 2, 2, 2, 2, 2, 2]]
 
-
-def people(code):
-    if code == 0:
-        return 1
-    else:
-        return 0
-
-
-def box(code):
-    if code == 1:
-        return 1
-    else:
-        return 0
-
-
-def isBoard(code):
-    if code == 2:
-        return 1
-    else:
-        return 0
-
-
-def isSlipBoard(code):
-    if code == 3:
-        return 1
-    else:
-        return 0
-
-
-def isBarrier(code):
-    if code == 4:
-        return 1
-    else:
-        return 0
-
-
-# def getplace(Map): #获取人物坐标
-#    for x in range(9):
-#        for y in range(8):
-#            if Map[x][y]==0:
-#                return x,y
 def getplace(Map):  # 获取人物坐标
-    for x in range(4):
-        for y in range(8):
-            if Map[x][y] == 0:  # list index out of range仍未解决
+    for x in range(5):
+        for y in range(9):
+            if Map[x][y] == 0:
                 return x, y
 
 
@@ -65,20 +24,46 @@ def willgo(x, y):  # 便于理解
     return Map[x][y]
 
 
-def move(standnow=2):
+def move():
     ls=[0,1,2,3]
     x, y = getplace(Map)
+    #排除无法进行的移动
     delete=[]
-    if x+1>3 or Map[x+1][y]==4:
+    if x+1>4 or Map[x+1][y]==4: #人物碰到箱子或障碍
+        delete.append(0)
+    elif x+2>4 and Map[x+1][y]==1: #箱子推出地图边界
+        delete.append(0)
+    elif Map[x+1][y]==1 and Map[x+2][y]==4: #箱子碰到障碍
+        delete.append(0)
+    elif Map[x+1][y]==1 and Map[x+2][y]==1: #箱子碰到箱子（两个箱子连在一起）
         delete.append(0)
     if x-1<0 or Map[x-1][y]==4:
         delete.append(1)
-    if y+1>7 or Map[x][y+1]==4:
+    elif x-2<0 and Map[x-1][y]==1:
+        delete.append(1)
+    elif Map[x-1][y]==1 and Map[x-2][y]==4:
+        delete.append(1)
+    elif Map[x-1][y]==1 and Map[x-2][y]==1:
+        delete.append(1)
+    if y+1>8 or Map[x][y+1]==4:
+        delete.append(2)
+    elif y+2>8 and Map[x][y+1]==1:
+        delete.append(2)
+    elif Map[x][y+1]==1 and Map[x][y+2]==4:
+        delete.append(2)
+    elif Map[x][y+1]==1 and Map[x][y+2]==1:
         delete.append(2)
     if y-1<0 or Map[x][y-1]==4:
         delete.append(3)
+    elif y-2<0 and Map[x][y-1]==1:
+        delete.append(3)
+    elif Map[x][y-1]==1 and Map[x][y-2]==4:
+        delete.append(3)
+    elif Map[x][y-1]==1 and Map[x][y-2]==1:
+        delete.append(3)
     for i in delete:
         ls.remove(i)
+    #排除无法进行的移动
     import random
     num = random.choice(ls)
     a = x
@@ -95,10 +80,18 @@ def move(standnow=2):
     elif num == 3:
         y -= 1
         act = "←"  # 向左
-    t = willgo(x, y)  # t用于保存将要前往的格子的属性，离开该格子后，该格子属性由0变为原属性
+    if willgo(x,y)==1:
+        if act=="↓":
+            Map[x+1][y]=1
+        elif act=="↑":
+            Map[x-1][y]=1
+        elif act=="→":
+            Map[x][y+1]=1
+        elif act=="←":
+            Map[x][y-1]=1
     Map[x][y] = 0
-    Map[a][b] = standnow  # 离开该格子后，该格子属性由0变为原属性
-    return t, act
+    Map[a][b] = bord[a][b]  # 离开该格子后，该格子属性由0变为原属性
+    return act
 
 
 #    elif willgo(x,y)==4: #遇到障碍
@@ -106,25 +99,33 @@ def move(standnow=2):
 #    elif willgo(x,y)==3: #滑块
 
 def goto(x, y):
-    a, b = getplace(Map)
+    a,b= getplace(Map)
     cnt = 0
     t = 2
     Step = ""
     while a != x or b != y:
-        t, step = move(t)
+        step = move()
         cnt += 1
         Step += step
-        a, b = getplace(Map)
+        a,b= getplace(Map)
+        if cnt>=30:
+            break
     return cnt, Step
 
 
 Min = 99999
-for i in range(10000):
-    Map = [[0, 2, 2, 4, 2, 2, 2, 2],
-           [4, 4, 2, 4, 2, 4, 4, 2],
-           [2, 4, 2, 4, 2, 2, 4, 2],
-           [2, 2, 2, 2, 2, 4, 2, 2]]
-    cnt, Step = goto(2, 5)
+for i in range(10000000000):
+    Map = [[4, 2, 1, 1, 1, 2, 1, 2, 2],
+           [2, 4, 2, 1, 1, 1, 1, 2, 4],
+           [4, 4, 2, 1, 2, 2, 2, 2, 4],
+           [4, 4, 1, 2, 1, 1, 1, 2, 4],
+           [0, 2, 2, 1, 1, 2, 2, 4, 4]]#物块类型数据
+    bord =[[4, 2, 2, 2, 2, 2, 2, 2, 2],
+           [2, 4, 2, 2, 2, 2, 2, 2, 4],
+           [4, 4, 2, 2, 2, 2, 2, 2, 4],
+           [4, 4, 2, 2, 2, 2, 2, 2, 4],
+           [2, 2, 2, 2, 2, 2, 2, 4, 4]] #地板类型数据
+    cnt, Step = goto(0, 8)
     if cnt < Min:
         Min = cnt
         Step0 = Step
@@ -140,3 +141,4 @@ print(Map0)
 # # 用户输入界面
 #
 # # 缺点：模间耦合度高
+
