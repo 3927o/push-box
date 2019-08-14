5c345aceba8e425eda9a14ea4ef2ba5408629bf9
3927o <53431636+3927o@users.noreply.github.com>
2019-08-14 22:52:01 +0800

    添加箱子滑块运动

--- 崩崩崩夏活.py
+++ 崩崩崩夏活.py
@@ -81,15 +81,47 @@ def move():
     elif num == 3:
         y -= 1
         act = "←"  # 向左
-    if willgo(x,y)==1:
+    if willgo(x,y)==1 and bord[x][y]==3:
+        x_box=x
+        y_box=y
+        m=1
+        while m!=0:
+            if act=="↓":
+                if x_box+1>=width or Map[x_box+1][y_box] in [1,4]:
+                    break
+                x_box+=1
+                if Map[x_box][y_box]==2:    break
+            elif act=="↑":
+                if x_box-1<0 or Map[x_box-1][y_box] in [1,4]:
+                    break
+                x_box-=1
+                if Map[x_box][y_box] == 2:    break
+            elif act=="→":
+                if y_box>=length or Map[x_box][y_box+1] in [1,4]:
+                    break
+                y_box+=1
+                if Map[x_box][y_box] == 2:    break
+            elif act=="←":
+                if y_box-1<0 or Map[x_box][y_box-1] in [1,4]:
+                    break
+                y_box-=1
+                if Map[x_box][y_box] == 2:    break
+        Map[x_box][y_box]=1
+        Map[x][y]=bord[x][y] #推箱子时人物位置未变化
+        return act
+    elif willgo(x,y)==1:
+        x_box=x
+        y_box=y
         if act=="↓":
-            Map[x+1][y]=1
+            Map[x_box+1][y_box]=1
         elif act=="↑":
-            Map[x-1][y]=1
+            Map[x_box-1][y_box]=1
         elif act=="→":
-            Map[x][y+1]=1
+            Map[x_box][y_box+1]=1
         elif act=="←":
-            Map[x][y-1]=1
+            Map[x_box][y_box-1]=1
+        Map[x][y]=bord[x][y]
+        return act
     if willgo(x,y)==3: #人滑块
         m=1
         while m!=0:
@@ -97,18 +129,22 @@ def move():
                 if x+1>=width or Map[x+1][y] in [1,4]:
                     break
                 x+=1
+                if Map[x][y] == 2:    break
             elif act=="↑":
                 if x-1<0 or Map[x-1][y] in [1,4]:
                     break
                 x-=1
+                if Map[x][y]==2:    break
             elif act=="→":
                 if y+1>=length or Map[x][y+1] in [1,4]:
                     break
                 y+=1
+                if Map[x][y] == 2:    break
             elif act=="←":
                 if y-1<0 or Map[x][y-1] in [1,4]:
                     break
                 y-=1
+                if Map[x][y] == 2:    break
     Map[x][y] = 0
     Map[a][b] = bord[a][b]  # 离开该格子后，该格子属性由0变为原属性
     return act
@@ -123,29 +159,33 @@ def goto(x, y):
         cnt += 1
         Step += step
         a,b= getplace(Map)
-        if cnt>=20:
+        if cnt>=100:
             break
     return cnt, Step
 
 Min = 99999
 for i in range(1000):
-    Map = [[3, 3, 3, 3, 4, 3, 3],
-           [3, 3, 3, 3, 3, 3, 3],
-           [4, 3, 3, 3, 3, 3, 3],
-           [3, 3, 3, 3, 3, 4, 3],
-           [3, 3, 3, 3, 3, 3, 3],
-           [3, 4, 0, 3, 3, 3, 3],
-           [4, 3, 3, 3, 3, 3, 3]]#物块类型数据
-    bord =[[3, 3, 3, 3, 3, 3, 3],
-           [3, 3, 3, 3, 3, 3, 3],
-           [3, 3, 3, 3, 3, 3, 3],
-           [3, 3, 3, 3, 3, 3, 3],
-           [3, 3, 3, 3, 3, 3, 3],
-           [3, 3, 3, 3, 3, 3, 3],
-           [3, 3, 3, 3, 3, 3, 3]] #地板类型数据
+    Map = [[4, 4, 2, 2, 2, 2, 2, 2],
+           [4, 2, 2, 2, 2, 2, 2, 2],
+           [4, 2, 2, 2, 4, 2, 2, 2],
+           [2, 2, 2, 4, 3, 3, 4, 2],
+           [2, 2, 3, 3, 3, 3, 2, 2],
+           [2, 2, 3, 3, 3, 3, 2, 2],
+           [2, 2, 3, 3, 3, 3, 2, 2],
+           [0, 1, 3, 4, 3, 4, 2, 2],
+           [2, 2, 2, 4, 2, 4, 4, 2]]#物块类型数据
+    bord =[[2, 2, 2, 2, 2, 2, 2, 2],
+           [4, 2, 2, 2, 2, 2, 2, 2],
+           [4, 2, 2, 2, 4, 2, 2, 2],
+           [2, 2, 2, 4, 3, 3, 4, 2],
+           [2, 2, 3, 3, 3, 3, 2, 2],
+           [2, 2, 3, 3, 3, 3, 2, 2],
+           [2, 2, 3, 3, 3, 3, 2, 2],
+           [2, 2, 3, 4, 3, 4, 2, 2],
+           [2, 2, 2, 4, 2, 4, 4, 2]] #地板类型数据
     width=len(Map)
     length=len(Map[0])
-    cnt, Step = goto(1, 4)
+    cnt, Step = goto(8, 4)
     if cnt < Min:
         Min = cnt
         Step0 = Step
@@ -153,9 +193,8 @@ for i in range(1000):
 print(Min, Step0)
 print(Map0)
 
-# 极待添加：箱子滑块
+# 代码复现严重，结构极差
 
-#
 
 # 有待添加块:
 # # 用户图形界面
